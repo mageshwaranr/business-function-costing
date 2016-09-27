ebf3841ddd5c3f5fc59f379bec590f827e294e55
Arunkumar S <asrisailapathi@vmware.com>
2016-09-27 18:37:55 +0530

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minify = require('gulp-minify');
+
+gulp.task('sassToCss', function() {
+   gulp .src('./src/sass/*.scss', {base: './src/sass'})
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('minifyJs', function() {
+    gulp .src('./src/js/*.js', {base: './src/js'})
+        .pipe(minify())
+        .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/js/*.js', ['minifyJs']);
+    gulp.watch('src/sass/*.scss', ['sassToCss']);
+});
+
+gulp.task('default', ['sassToCss', 'minifyJs']);
---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+var CostPricing = CostPricing || {};
+
+CostPricing.module = (function () {
+    var fn, api;
+    
+    fn = {
+        _init: function () {
+            
+        }
+    };
+    
+    api = {
+        init: function () {
+            return fn._init.apply(this, arguments);
+        }
+    };
+    
+    return api;
+})();
